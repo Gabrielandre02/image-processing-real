79872e82548a68300fc1745face90e6159361ba9
gabriel andre <andre-carmo02@hotmail.com>
2022-03-16 19:33:58 -0300

    att

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ with open("requirements.txt") as f:
 
 setup(
     name="image_processing",
-    version="0.0.2",
+    version="0.0.1",
     author="gabriel",
     author_email="andre-carmo02@hotmail.com",
     description="Versão de teste - Processamento de imagem. Este projeto pertence a Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist da Take. Este pacote é uma demonstração para simulação de upload no site Test Pypi, e é da classe do desenvolvedor do Bootcamp . E-mail: karinatkato@gmail.com.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Gabrielandre02/image-processing"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
